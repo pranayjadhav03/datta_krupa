f45d973a2019e1373339cdf1f87b7879da1798eb
pranayjadhav03 <jadhavpranay404@gmail.com>
2025-07-21 00:52:29 +0530

    Fix: login in setting

--- datta_krupa/settings.py
+++ datta_krupa/settings.py
@@ -110,4 +110,8 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+# Auth Redirects
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/accounts/login/'
+LOGIN_URL = '/accounts/login/'
 
